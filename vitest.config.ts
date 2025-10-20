eca884b9b93aabb0373353a59a2ca77d61ce5b0d
TsitouhRanjafy <radohery09@gmail.com>
2025-10-20 16:39:53 +0300

    setup env for test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config'
 
+
 export default defineConfig({
   test: {
     include: [
@@ -7,7 +8,17 @@ export default defineConfig({
         '__test__/**/*.test.ts',
         'src/**/*.test.ts'
     ],
-    testTimeout: 5000
+    testTimeout: 5000,
+    env: {
+      "PORT":"3000",
+      "DATABASE_URL":"mysql://tsitohaina:Html2025@localhost:3306/drop_shop",
+      "JWT_KEY_SECRET":"KEY_JWT",
+      "TOKEN_KEY_SECRET":"TOKEN_KEY",
+      "CORS_ORIGIN1":"http://localhost:4200",
+      "CORS_ORIGIN2":"http://localhost:4200",
+      "CORS_ORIGIN3":"http://localhost:4200",
+      "CORS_ORIGIN4":"http://localhost:4200"
+    }
   },
   resolve: {
     alias: {
