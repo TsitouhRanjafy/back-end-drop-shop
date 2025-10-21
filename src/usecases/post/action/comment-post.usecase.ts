ff69498cee8ad5497112773556498d82656463e3
TsitouhRanjafy <radohery09@gmail.com>
2025-10-21 15:31:51 +0300

    fix: server crashed when user of post not found

--- src/usecases/post/action/comment-post.usecase.ts
+++ src/usecases/post/action/comment-post.usecase.ts
@@ -1,13 +1,25 @@
 import { IComment } from "../../../domain/entities/comment.interface";
-import { ISaveCommentRepository } from "../../../domain";
+import { ILoadPostRepository, ILoadUserRepository, ISaveCommentRepository } from "../../../domain";
 
 export default class CommentPostUsecase {
     constructor(
-        private saveCommentRepository: ISaveCommentRepository
+        private saveCommentRepository: ISaveCommentRepository,
+        private loadPostRepository: ILoadPostRepository,
+        private loadUserRepository: ILoadUserRepository
     ){}
 
     async exec(comment: Omit<IComment,"id" | "date">): Promise<IComment | null>{
-        const newComment = await this.saveCommentRepository.addComment(comment);
-        return newComment;
+        try {
+            const thisPostExist = await this.loadPostRepository.getPostById(comment.id_post);
+            if (!thisPostExist) return null;
+
+            const thisUserExist = await this.loadUserRepository.getUserById(comment.id_user, false);
+            if  (!thisUserExist) return null;
+    
+            const newComment = await this.saveCommentRepository.addComment(comment);
+            return newComment;
+        } catch (error) {
+            return null;
+        }
     }
 }
