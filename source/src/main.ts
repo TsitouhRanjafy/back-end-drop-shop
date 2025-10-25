76179206a8463682fe7abbefd14a8adc0b1c3e28
TsitouhRanjafy <radohery09@gmail.com>
2025-10-25 18:23:19 +0300

    update: use httpServer instead of express app

--- source/src/main.ts
+++ source/src/main.ts
@@ -1,7 +1,9 @@
 import env from "./configuration/env.config";
 import { app } from "./configuration/setup-app.config";
 
-app.listen(env.port, () => {
+// Récupérer le httpServer attaché dans socket.config.ts
+const httpServer = app.get('httpServer'); 
+httpServer.listen(env.port, () => {
     console.log(` \nServer running on http://localhost:${env.port}`);
 })
 
