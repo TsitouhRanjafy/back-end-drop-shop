b75705ce1c70681d1844b8768534c09666317b59
TsitouhRanjafy <radohery09@gmail.com>
2025-10-25 19:12:47 +0300

    feat: WIP

--- source/src/controllers/message/message.controller.ts
+++ source/src/controllers/message/message.controller.ts
@@ -13,22 +13,54 @@ export class MessageController {
 
     private intializeSocketEvents(): void {
         this.io.on('connection', (socket: Socket) => {
-            socket.on('sendMessage', (arg: unknown) => {
-                console.log(arg);
 
-                const message: Omit<IMessage,'id' | 'sending_date' | 'room_id'> | null = this.formerService.parsePartialMessage(arg);
-                if (!message) {
-                    console.log("message invalide");
+            // join a room
+            socket.on('on_join_room', (arg: unknown) => {
+                const room: string | null = ((): string | null => {
+                    if (typeof arg !== "object" || arg === null) return null;
+
+                    if (typeof (arg as any).id_expediteur !== "number") return null;
+                    if (typeof (arg as any).id_receiveur !== "number") return null;
+                    const { id_expediteur, id_receiveur } = arg as { id_expediteur: number, id_receiveur: number };
+
+                    return this.formerService.concateByPriorty(id_expediteur, id_receiveur);
+                })();
+
+                if (!room) {
+                    socket.emit('error', { message: "bad id_expediteur or id_receiveur" });
                     return;
                 }
+                socket.join(room);
+            });
 
-                const room: string = this.formerService.concateByPriorty(message.id_expediteur, message.id_receiveur);
-                console.log(room);
+            // send message
+            socket.on('sendMessage', async (arg: unknown) => {
+                try {
+                    console.log(arg);
 
-                const newMessage: Omit<IMessage,"id" | "sending_date"> | null = this.formerService.parseMessage(message);
-                console.log(newMessage);
+                    const message: Omit<IMessage,'id' | 'sending_date' | 'room_id'> | null = this.formerService.parsePartialMessage(arg);
+                    if (!message) {
+                        socket.emit('error', { message: 'bad id_expediteur or id_receiveur or content' });
+                        return;
+                    }
+                    const room: string = this.formerService.concateByPriorty(message.id_expediteur, message.id_receiveur);
+                    console.log(room);
 
-                
+                    const newMessage: Omit<IMessage,"id" | "sending_date"> | null = this.formerService.parseMessage(message);
+                    console.log(newMessage);
+                    if (!newMessage) return;
+
+
+                    const savedMessage: IMessage | null = await this.saveMessageUsecase.exec(newMessage);
+                    if (!savedMessage) {
+                        this.io.to(room).emit('on_message_not_saved');
+                        return;
+                    }
+                    this.io.to(room).emit('on_receive_message', newMessage);
+                } catch (error) {
+                    console.error('Error on sendMessage: ', error);
+                    socket.emit('error', { message: 'Internal serveur error' });
+                }
             })
         })
     }
