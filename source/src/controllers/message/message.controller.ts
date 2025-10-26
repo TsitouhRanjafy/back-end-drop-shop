326a8cfe1cbb9f685d46b8e78c9258bd03fb0172
TsitouhRanjafy <radohery09@gmail.com>
2025-10-26 20:22:37 +0300

    feat: handler socket event

--- source/src/controllers/message/message.controller.ts
+++ source/src/controllers/message/message.controller.ts
@@ -13,54 +13,61 @@ export class MessageController {
 
     private intializeSocketEvents(): void {
         this.io.on('connection', (socket: Socket) => {
-
-            // join a room
-            socket.on('on_join_room', (arg: unknown) => {
+            console.log("un client est connecté, socket_id: "+socket.id);
+            
+            socket.on('join_room', (arg: unknown) => {
+                
                 const room: string | null = ((): string | null => {
                     if (typeof arg !== "object" || arg === null) return null;
 
-                    if (typeof (arg as any).id_expediteur !== "number") return null;
-                    if (typeof (arg as any).id_receiveur !== "number") return null;
-                    const { id_expediteur, id_receiveur } = arg as { id_expediteur: number, id_receiveur: number };
-
+                    if (typeof parseInt((arg as any).id_expediteur) !== "number") return null;
+                    if (typeof parseInt((arg as any).id_receiveur) !== "number") return null;
+                    const { id_expediteur, id_receiveur } = arg as { 
+                        id_expediteur: number, 
+                        id_receiveur: number 
+                    };
+                    
                     return this.formerService.concateByPriorty(id_expediteur, id_receiveur);
                 })();
 
                 if (!room) {
-                    socket.emit('error', { message: "bad id_expediteur or id_receiveur" });
+                    socket.emit('error', { error_message: "bad id_expediteur or id_receiveur" });
                     return;
                 }
                 socket.join(room);
+                console.log('room_id: ',room,' is joined by user_id:'+((arg as any).id_expediteur)); 
             });
 
-            // send message
-            socket.on('sendMessage', async (arg: unknown) => {
+            socket.on('send_message', async (arg: unknown) => {
                 try {
-                    console.log(arg);
-
                     const message: Omit<IMessage,'id' | 'sending_date' | 'room_id'> | null = this.formerService.parsePartialMessage(arg);
                     if (!message) {
-                        socket.emit('error', { message: 'bad id_expediteur or id_receiveur or content' });
+                        socket.emit('error', { error_message: 'bad id_expediteur or id_receiveur or content' });
                         return;
                     }
+
                     const room: string = this.formerService.concateByPriorty(message.id_expediteur, message.id_receiveur);
-                    console.log(room);
-
-                    const newMessage: Omit<IMessage,"id" | "sending_date"> | null = this.formerService.parseMessage(message);
+                    const newMessage: Omit<IMessage,"id" | "sending_date"> | null = this.formerService.parseMessage({...message, room_id: room});
                     console.log(newMessage);
-                    if (!newMessage) return;
-
+                    if (!newMessage) {
+                        socket.emit('error', { error_message: 'something whent wrong'});
+                        return;
+                    };
 
                     const savedMessage: IMessage | null = await this.saveMessageUsecase.exec(newMessage);
                     if (!savedMessage) {
-                        this.io.to(room).emit('on_message_not_saved');
+                        socket.emit('error', { error_message: 'failed to save message' });
                         return;
                     }
-                    this.io.to(room).emit('on_receive_message', newMessage);
+                    socket.to(room).emit('on_receive_message', savedMessage);
                 } catch (error) {
                     console.error('Error on sendMessage: ', error);
-                    socket.emit('error', { message: 'Internal serveur error' });
+                    socket.emit('error', { error_message: 'Internal serveur error' });
                 }
+            })
+
+            socket.on('get_room_history', async (arg: unknown) => {
+                /* not implemented */
             })
         })
     }
